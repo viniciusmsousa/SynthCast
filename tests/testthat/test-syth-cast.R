784a4bace9e3fe86525ce25f346492faf387b48b
ViniciusMSousa <vinisousa04@gmail.com>
2021-06-07 20:49:36 -0300

    tests: prepare_dataset()

--- tests/testthat/test-syth-cast.R
+++ tests/testthat/test-syth-cast.R
@@ -15,6 +15,14 @@ elegible <- intern_elegile_units(
   periods_to_forecast = 6
 )
 
+prepared_df <- prepare_dataset(
+  df = df_example,
+  df_elegible_units = elegible,
+  col_unit_name = 'unit',
+  col_time = 'time_period',
+  unit_of_interest = 30,
+  max_time_unit_of_interest = max_time_unit
+)
 
 # Tests -------------------------------------------------------------------
 
@@ -46,6 +54,35 @@ test_that(
     expect_equal(
       object = as.vector(sapply(elegible, class)),
       expected = c("integer", "logical")
+    )
+  }
+)
+
+test_that(
+  desc = "Tests prepare_dataset()",
+  code = {
+    expect_equal(
+      object = class(prepared_df),
+      expected = c("data.frame")
+    )
+
+    expect_equal(
+      object = c(ncol(prepared_df), nrow(prepared_df)),
+      expected = c(31, 525)
+    )
+
+    expect_equal(
+      object = as.vector(sapply(prepared_df, class)),
+      expected = c(
+        "character", "integer", "integer", "numeric",
+        "numeric", "numeric", "numeric", "numeric",
+        "numeric", "numeric", "numeric", "numeric",
+        "numeric", "numeric", "numeric", "numeric",
+        "numeric", "numeric", "numeric", "numeric",
+        "numeric", "numeric", "numeric", "numeric",
+        "numeric", "numeric", "numeric", "numeric",
+        "numeric", "numeric", "numeric"
+      )
     )
   }
 )
